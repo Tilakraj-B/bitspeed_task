1087ec87fc39b52af6417c22074a8894f392418a
Tilakraj Baghel <tilakrajbaghel4@gmail.com>
2025-05-27 10:02:03 +0530

    Routing done

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+app.use(express.json());
+app.use('/', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
